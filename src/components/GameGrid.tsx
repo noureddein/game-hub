e203273bebf45795c0d6ccaf5f435cc4709cd2a0
noureddein <noureddein@gmail.com>
2023-04-11 03:59:11 +0300

    Fetch the games

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,38 @@
+﻿import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { Text } from "@chakra-ui/react";
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGameResponse {
+    count: number;
+    results: Game[];
+}
+
+function GameGrid() {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        apiClient
+            .get<FetchGameResponse>("/games")
+            .then((res) => setGames(res.data.results))
+            .catch((err) => setError(err.message));
+    }, []);
+
+    return (
+        <>
+            {error && <Text>{error}</Text>}
+            <ul>
+                {games.map(({ id, name }) => (
+                    <li key={id}>{name}</li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default GameGrid;
---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+﻿import axios from "axios";
+
+export default axios.create({
+    baseURL: "https://api.rawg.io/api",
+    params: {
+        key: "cf3f90f4a42e40f9a1c85e656398015d",
+    },
+});
