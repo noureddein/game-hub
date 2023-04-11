a9e21bab576dfb948404a498b539f4b85ed0554a
noureddein <noureddein@gmail.com>
2023-04-11 03:09:42 +0300

    Build a Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+﻿import { HStack, Image, Text } from '@chakra-ui/react'
+import logo from '../assets/logo.webp';
+
+export const Navbar = () => {
+  return (
+    <HStack>
+        <Image src={logo} alt='logo' boxSize='60px'/>
+        <Text>Navbar</Text>
+    </HStack>
+  )
+}
