d0a977c03f74b6eb8d48f913701b9da672ea6392
noureddein <noureddein@gmail.com>
2023-04-14 01:17:57 +0300

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,15 +7,17 @@ import { Game } from "../hooks/useGames";
 // ** Components import
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
     game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
+    
     return (
         <Card borderRadius={10} overflow={"hidden"}>
-            <Image src={game.background_image} />
+            <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize="2xl">{game.name}</Heading>
                 <HStack justifyContent="space-between">
---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+﻿// https://media.rawg.io/media/games/d58/d588947d4286e7b5e0e12e1bea7d9844.jpg
+const getCroppedImageUrl = (url: string) => {
+    const TARGET = "media/";
+
+    const index = url.indexOf(TARGET) + TARGET.length;
+    return `${url.slice(0, index)}crop/600/400/${url.slice(index)}`;
+};
+
+export default getCroppedImageUrl;
