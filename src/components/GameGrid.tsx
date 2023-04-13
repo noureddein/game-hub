267c2bdac482ee451b69d64ae03b794c6154469c
noureddein <noureddein@gmail.com>
2023-04-14 01:47:50 +0300

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,17 +2,26 @@
 
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 function GameGrid() {
-    const { games, error } = useGames();
+    const { games, error, isLoading } = useGames();
+    const skeletons = [1, 2, 3, 4, 5, 6];
 
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 ,"2xl": 5}} spacing={10} padding='10px'>
-                {games.map((game) => (
+            <SimpleGrid
+                columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+                spacing={10}
+                padding="10px">
+                {true &&
+                    skeletons.map((skeleton) => (
+                        <GameCardSkeleton key={skeleton} />
+                    ))}
+                {/* {games.map((game) => (
                     <GameCard game={game} key={game.id} />
-                ))}
+                ))} */}
             </SimpleGrid>
         </>
     );
